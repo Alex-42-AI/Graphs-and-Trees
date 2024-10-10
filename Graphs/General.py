30e07baf3f7dcfc8fd780b1e16ca93607c5716f7
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-10-10 20:30:10 +0300

    Update General.py

--- Graphs/General.py
+++ Graphs/General.py
@@ -8,11 +8,9 @@ class Dict:
             if arg not in self.__items:
                 if len(arg) != 2:
                     raise ValueError("Pairs of keys and values expected!")
-                self.__items.append(arg)
-        for i, item1 in enumerate(self.__items):
-            for item2 in self.__items[i + 1:]:
-                if item1[0] == item2[0]:
+                if arg[0] in self:
                     raise KeyError("No similar keys in a dictionary allowed!")
+                self.__items.append(arg)
                     
     def keys(self):
         return [p[0] for p in self.items()]
@@ -94,10 +92,9 @@ class SortedKeysDict(Dict):
             if arg not in self.__items:
                 if len(arg) != 2:
                     raise ValueError("Pairs of keys and values expected!")
+                if arg[0] in self:
+                    raise KeyError("No similar keys in a dictionary allowed!")
                 self[arg[0]] = arg[1]
-        for i in range(len(self.__items) - 1):
-            if self.__items[i][0] == self.__items[i + 1][0]:
-                raise KeyError("No similar keys in a dictionary allowed!")
                 
     def f(self, x=None):
         return self.__f if x is None else self.__f(x)
