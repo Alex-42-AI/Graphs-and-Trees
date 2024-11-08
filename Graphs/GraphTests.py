a3b630201fd1606a4676b183a63dd54290562937
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-11-09 01:07:17 +0200

    Update GraphTests.py

--- Graphs/GraphTests.py
+++ Graphs/GraphTests.py
@@ -3,7 +3,7 @@ from Graphs.UndirectedGraph import *
 from Graphs.DirectedGraph import *
 from Graphs.Tree import *
 
-n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16, n17, n18, n19, n20, n21 = Node(0), Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7), Node(8), Node(9), Node(10), Node(11), Node(12), Node(13), Node(14), Node(15), Node(16), Node(17), Node(18), Node(19), Node(20), Node(21)
+n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15 = Node(0), Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7), Node(8), Node(9), Node(10), Node(11), Node(12), Node(13), Node(14), Node(15)
 
 
 class TestUndirectedGraph(TestCase):
