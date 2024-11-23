6f73a7c10759fe2f1a59890983a5870a19d57d12
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-11-23 22:06:17 +0200

    Update main.py

--- Graphs/main.py
+++ Graphs/main.py
@@ -107,25 +107,6 @@ def make_undirected_from_directed(graph: DirectedGraph):
     return res
 
 
-def heapify(ll: list[int], l: int, h: int, i: int, f=max):
-    left, right = 2 * i - l, 2 * i - l + 1
-    res = i
-    if left <= h and (el := ll[i - 1]) != f(ll[left - 1], el):
-        res = left
-    if right <= h and (el := ll[res - 1]) != f(ll[right - 1], el):
-        res = right
-    if res != i:
-        ll[i - 1], ll[res - 1] = ll[res - 1], ll[i - 1]
-        heapify(ll, res - l - 1, h, res, f)
-
-
-def build_heap(ll: list[int], h: int = 0):
-    if not h:
-        h = len(ll)
-    for i in range(h // 2, 0, -1):
-        heapify(ll, 0, h, i)
-
-
 def binary_heap(l: list):
     build_heap(l, len(l))
 
