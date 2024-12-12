90abcb3b985bfac45908384f63d2a273e6f351af
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-12-13 00:38:58 +0200

    Update DirectedGraph.py

--- Graphs/DirectedGraph.py
+++ Graphs/DirectedGraph.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 from itertools import permutations, product
 
-from Personal.DiscreteMath.Graphs.General import *
+from Graphs.General import *
 
 
 class DirectedGraph(Graph):
