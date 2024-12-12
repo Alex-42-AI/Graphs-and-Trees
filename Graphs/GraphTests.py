982b93f6fe8b54f3d1eb0be97654a9747909bc26
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-12-13 00:50:33 +0200

    Update GraphTests.py

--- Graphs/GraphTests.py
+++ Graphs/GraphTests.py
@@ -1,10 +1,10 @@
 from unittest import TestCase, main
 
-from .UndirectedGraph import *
+from Graphs.UndirectedGraph import *
 
-from .DirectedGraph import *
+from Graphs.DirectedGraph import *
 
-from .Tree import *
+from Graphs.Tree import *
 
 
 n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15 = Node(0), Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7), Node(8), Node(9), Node(10), Node(11), Node(12), Node(13), Node(14), Node(15)
