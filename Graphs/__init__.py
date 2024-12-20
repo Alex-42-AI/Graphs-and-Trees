e6595a7a7ea028840f9e31ddffd9ecc71d452f11
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-12-20 07:31:48 +0200

    Update __init__.py

--- Graphs/__init__.py
+++ Graphs/__init__.py
@@ -1,7 +1,7 @@
-from Personal.Graphs.src.undirected_graph import *
+from Graphs.src.undirected_graph import *
 
-from Personal.Graphs.src.directed_graph import *
+from Graphs.src.directed_graph import *
 
-from Personal.Graphs.src.tree import *
+from Graphs.src.tree import *
 
-from Personal.Graphs.src.main import clique_to_SAT, build_heap, binary_heap, print_zig_zag
+from Graphs.src.main import clique_to_SAT, build_heap, binary_heap, print_zig_zag
