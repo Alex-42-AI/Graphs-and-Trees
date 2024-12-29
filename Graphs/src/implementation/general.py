efc072f82bee7484d8d2db13a53271a5d45d4ad2
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-12-29 17:01:07 +0200

    Update general.py

--- Graphs/src/implementation/general.py
+++ Graphs/src/implementation/general.py
@@ -14,7 +14,8 @@ class Node:
     @property
     def value(self):
         """
-        Get value.
+        Returns:
+            Node value.
         """
         return self.__value
 
@@ -24,27 +25,27 @@ class Node:
     def __hash__(self) -> int:
         return hash(self.value)
 
-    def __eq__(self, other) -> bool:
-        if isinstance(other, Node):
+    def __eq__(self, other: "Node") -> bool:
+        if type(other) == Node:
             return self.value == other.value
         return False
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: "Node") -> bool:
         if isinstance(other, Node):
             return self.value < other.value
         return self.value < other
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: "Node") -> bool:
         if isinstance(other, Node):
             return self.value <= other.value
         return self.value <= other
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: "Node") -> bool:
         if isinstance(other, Node):
             return self.value >= other.value
         return self.value >= other
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: "Node") -> bool:
         if isinstance(other, Node):
             return self.value > other.value
         return self.value > other
@@ -63,21 +64,26 @@ class Graph(ABC):
     @abstractmethod
     def nodes(self) -> set[Node]:
         """
-        Get nodes.
+        Returns:
+            Graph nodes.
         """
         pass
 
     @abstractmethod
     def links(self) -> set:
         """
-        Get links.
+        Returns:
+            Graph links.
         """
         pass
 
     @abstractmethod
-    def degrees(self, n: Node = None) -> dict | int:
+    def degrees(self, u: Node = None) -> dict | int:
         """
-        Get degree of a given node or a dictionary with all node degrees.
+        Args:
+            u: Node object or None.
+        Returns:
+            Node degree or dictionary with all node degrees.
         """
         pass
 
@@ -85,206 +91,265 @@ class Graph(ABC):
     def remove(self, n: Node, *rest: Node) -> "Graph":
         """
         Remove a non-empty set of nodes.
+        Args:
+            n: First given node.
+            rest: Other given nodes.
         """
         pass
 
     @abstractmethod
     def connect_all(self, u: Node, *rest: Node) -> "Graph":
         """
-        Add a link between all given nodes.
+        Args:
+            u: Node object.
+            rest: Node objects.
+        Connect every given node to every other given node, all present in the graph.
         """
         pass
 
     @abstractmethod
     def disconnect_all(self, u: Node, *rest: Node) -> "Graph":
         """
-        Disconnect all given nodes.
+        Args:
+            u: First given node.
+            rest: Other given nodes.
+        Disconnect all given nodes, all present in the graph.
         """
         pass
 
     @abstractmethod
     def copy(self) -> "Graph":
         """
-        Return identical copy of the graph.
+        Returns:
+            Identical copy of the graph.
         """
         pass
 
     @abstractmethod
     def complementary(self) -> "Graph":
         """
-        Return a graph, where there are links between nodes exactly
-        where there are no links between nodes in the original graph.
+        Returns:
+            A graph, where there are links between nodes exactly where there aren't in the original graph.
         """
         pass
 
     @abstractmethod
     def component(self, u: Node) -> "Graph":
         """
-        Return the weakly connected component, to which a given node belongs.
+        Args:
+            u: Given node.
+        Returns:
+            The weakly connected component, to which a given node belongs.
         """
         pass
 
     @abstractmethod
     def subgraph(self, u_or_s: Node | Iterable[Node]) -> "Graph":
         """
-        If a set of nodes is given, return the subgraph, that only contains these nodes and all links between them.
-        If a node is given, return the subgraph of all nodes and links, reachable by it, in a directed graph.
+        Args:
+            u_or_s: Given node or set of nodes.
+        Returns:
+            If a set of nodes is given, return the subgraph, that only contains these nodes and all links between them.
+            If a node is given, return the subgraph of all nodes and links, reachable by it, in a directed graph.
         """
         pass
 
     @abstractmethod
     def connection_components(self) -> "list[Graph]":
         """
-        List out all connected components of the graph.
+        Returns:
+            A list of all connection components of the graph.
         """
         pass
 
     @abstractmethod
     def connected(self) -> bool:
         """
-        Check whether the graph is connected.
+        Returns:
+            Whether the graph is connected.
         """
         pass
 
     @abstractmethod
     def reachable(self, u: Node, v: Node) -> bool:
         """
-        Check whether the first given node can reach the second one.
+        Args:
+            u: First given node.
+            v: Second given node.
+        Returns:
+            Whether the first given node can reach the second one.
         """
         pass
 
     @abstractmethod
     def full(self) -> bool:
         """
-        Check whether the graph is fully connected.
+        Returns:
+            Whether the graph is fully connected.
         """
         pass
 
     @abstractmethod
     def get_shortest_path(self, u: Node, v: Node) -> list[Node]:
         """
-        Return one shortest path from the first given node to the second one, if such path exists, otherwise empty list.
+        Args:
+            u: First given node.
+            v: Second given node.
+        Returns:
+            One shortest path from u to v, if such path exists, otherwise empty list.
         """
         pass
 
     @abstractmethod
     def euler_tour_exists(self) -> bool:
         """
-        Check whether an Euler tour exists.
+        Returns:
+            Whether an Euler tour exists.
         """
         pass
 
     @abstractmethod
     def euler_walk_exists(self, u: Node, v: Node) -> bool:
         """
-        Check whether an Euler walk exists.
+        Args:
+            u: First given node.
+            v: Second given node.
+        Returns:
+            Whether an Euler walk from u to v exists.
         """
         pass
 
     @abstractmethod
     def euler_tour(self) -> list[Node]:
         """
-        Return an Euler tour, if such exists, otherwise empty list.
+        Returns:
+             An Euler tour, if such exists, otherwise empty list.
         """
         pass
 
     @abstractmethod
     def euler_walk(self, u: Node, v: Node) -> list[Node]:
         """
-        Return an Euler walk, if such exists, otherwise empty list.
+        Args:
+            u: First given node.
+            v: Second given node.
+        Returns:
+             An Euler walk, if such exists, otherwise empty list.
         """
         pass
 
     @abstractmethod
     def path_with_length(self, u: Node, v: Node, length: int) -> list[Node]:
         """
-        Return the path from the first given node to the second one, if such path exists, otherwise empty list.
+        Args:
+            u: First given node.
+            v: Second given node.
+            length: Length of the wanted path.
+        Returns:
+            A path from u to v with given length, if such path exists, otherwise empty list.
         """
         pass
 
     @abstractmethod
     def cycle_with_length(self, length: int) -> list[Node]:
         """
-        Return a cycle with given length, if such exists, otherwise empty list.
+        Args:
+            length: Length of the wanted cycle.
+        Returns:
+            A cycle with given length, if such exists, otherwise empty list.
         """
         pass
 
     @abstractmethod
     def hamilton_tour_exists(self) -> bool:
         """
-        Check whether a Hamilton tour exists.
+        Returns:
+            Whether a Hamilton tour exists.
         """
         pass
 
     @abstractmethod
     def hamilton_walk_exists(self, u: Node, v: Node) -> bool:
         """
-        u: first given node
-        v: second given node
-        Check whether a Hamilton walk exists from the first given node to the second one.
+        Args:
+            u: first given node
+            v: second given node
+        Returns:
+            Whether a Hamilton walk exists from u to v.
         """
         pass
 
     @abstractmethod
     def hamilton_tour(self) -> list[Node]:
         """
-        Return a Hamilton tour, if such exists, otherwise empty list.
+        Returns:
+            A Hamilton tour, if such exists, otherwise empty list.
         """
         pass
 
     @abstractmethod
-    def hamilton_walk(self, u: Node, v: Node) -> list[Node]:
+    def hamilton_walk(self, u: Node = None, v: Node = None) -> list[Node]:
         """
-        u: first given node
-        v: second given node
-        Return a Hamilton walk from node u to node v, if such exists, otherwise empty list.
+        Args:
+            u: first given node or None.
+            v: second given node or None.
+        Returns:
+            A Hamilton walk from u to v, if such exists, otherwise empty list.
+            If a node isn't given, it could be any.
         """
         pass
 
     @abstractmethod
     def isomorphic_bijection(self, other: "Graph") -> dict[Node, Node]:
         """
-        other: another Graph object
-        Return an isomorphic function between the nodes of the graph and those of the
-        given graph, if such exists, otherwise empty dictionary. Let f be such a bijection
-        and u and v be nodes in the graph. f(u) and f(v) are nodes in the other graph and
-        f(u) and f(v) are neighbors (or f(u) points to f(v) for directed graphs) exactly when
-        the same applies for u and v. For weighted graphs, the weights are taken into account.
+        Args:
+            other: another Graph object
+        Returns:
+            An isomorphic function (bijection) between the nodes of the graph and those of the
+            given graph, if such exists, otherwise empty dictionary. Let f be such a bijection
+            and u and v be nodes in the graph. f(u) and f(v) are nodes in the other graph and
+            f(u) and f(v) are neighbors (or f(u) points to f(v) for directed graphs) exactly when
+            the same applies for u and v. For weighted graphs, the weights are taken into account.
         """
         pass
 
     @abstractmethod
     def __bool__(self) -> bool:
         """
-        Check whether the graph has nodes.
+        Returns:
+            Whether the graph has nodes.
         """
         pass
 
     @abstractmethod
     def __reversed__(self) -> "Graph":
         """
-        Complementary graph.
+        Returns:
+            Complementary graph.
         """
         pass
 
     @abstractmethod
     def __contains__(self, u: Node) -> bool:
         """
-        Check whether a given node is in the graph.
+        Args:
+            u: item.
+        Returns:
+            Whether u is a node in the graph.
         """
         pass
 
     @abstractmethod
     def __add__(self, other: "Graph") -> "Graph":
-        """
-        Combine two graphs.
-        """
         pass
 
     @abstractmethod
     def __eq__(self, other: "Graph"):
         """
-        Check whether two graphs are equal.
+        Args:
+            other: another Graph object
+        Returns:
+            Whether both graphs are equal.
         """
         pass
 
