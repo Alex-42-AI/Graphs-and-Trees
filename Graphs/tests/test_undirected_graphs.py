9c5ac5d895eadb50aaea7d9a7db16471baa002ca
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-01-06 20:46:53 +0200

    Update test_undirected_graphs.py

--- Graphs/tests/test_undirected_graphs.py
+++ Graphs/tests/test_undirected_graphs.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 
-from Personal.Graphs.src.implementation.undirected_graph import *
+from Graphs.src.undirected_graph import *
 
 n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15 = Node(0), Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7), Node(8), Node(9), Node(10), Node(11), Node(12), Node(13), Node(14), Node(15)
 
